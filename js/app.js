a5f415969f8cbb33a16dc21d55c6949803099055
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-06-06 11:00:48 -0500

    input is taken and logged in the console and also clears after logged

--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@ console.log("form inputs and to-to list");
 // grab button with id #add-item
 const addItemButton = document.querySelector('#add-item')
 
-
+// listen for 'submit' instead of 'click'
 const itemAddingForm = document.querySelector('#item-adding-form')
 itemAddingForm.addEventListener('submit', (event) => {
   event.preventDefault() // stops form submission
-  console.log("form submitted now");
+  // find the input
+  const itemTextInput = document.querySelector('#item-text-input')
+  // get the text
+  // log that text in the console for now
+  console.log(itemTextInput.value);
+  // clear the form
+  itemTextInput.value = ""
+
 })
 
 
