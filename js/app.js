6bd33424d22b52a778ae5834e23dcd61159e6502
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-06-06 10:48:21 -0500

    input box and button to add, and event listener for clicking button set up and working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log("form inputs and to-to list");
+
+// grab button with id #add-item
+const addItemButton = document.querySelector('#add-item')
+
+// add listener to the button that listens for 'click'
+// and give it an AF to run
+addItemButton.addEventListener('click', () => {
+  console.log("the add item button was clicked");
+})
