7d405c778821812c0264a9aaee734612056cb682
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-06-06 10:57:00 -0500

    adding form and eventDefault

--- js/app.js
+++ js/app.js
@@ -3,8 +3,11 @@ console.log("form inputs and to-to list");
 // grab button with id #add-item
 const addItemButton = document.querySelector('#add-item')
 
-// add listener to the button that listens for 'click'
-// and give it an AF to run
-addItemButton.addEventListener('click', () => {
-  console.log("the add item button was clicked");
+
+const itemAddingForm = document.querySelector('#item-adding-form')
+itemAddingForm.addEventListener('submit', (event) => {
+  event.preventDefault() // stops form submission
+  console.log("form submitted now");
 })
+
+
